7ac691b8cba0d5d1db00ce8cdd57923f176b7e60
Ronald Salazar M <rasm200@gmail.com>
2022-09-25 16:09:55 -0600

    primer commit

---
+++ modules.js
@@ -0,0 +1,32 @@
+
+//const MiModulo = (()=>{
+   'use strict';
+
+   const Sumar = (a,b)=>{
+        alert(a+b);
+    }
+    
+    const Restar = (a,b)=>{
+        alert(a-b);
+    }
+    
+    const Multiplicar = (a,b)=>{
+        alert(a*b);
+    }
+    
+    const Dividir = (a,b)=>{
+        return(a/b);
+    }
+
+    const MetResultados = (resultado)=>{
+        return resultado;
+    }
+   
+//})()
+
+export  {
+    Dividir
+}
+
+
+
